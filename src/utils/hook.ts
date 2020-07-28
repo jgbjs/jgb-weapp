90f33ef7c8b64d7f07f157801b1d958282c45174
landn172 <landn172@gmail.com>
2020-07-28 16:56:10 +0800

    fix(jcomponent): 针对Component做lifetime做兼容

--- src/utils/hook.ts
+++ src/utils/hook.ts
@@ -3,19 +3,24 @@ import { IEventFunction } from '../../types/eventbus';
 
 /**
  * hook function
+ * @param obj 
+ * @param key 
+ * @param fn 
+ * @param hookFirst {Boolean} hook的方法优先执行
  */
-export function hook(obj: any, key: string, fn: IEventFunction) {
+export function hook(obj: any, key: string, fn: IEventFunction, hookFirst = true) {
   // tslint:disable-next-line: no-empty
   const oldFn = obj[key] || (() => {});
   if (!isFunction(fn)) {
     throw new Error(`cannot hook [${key}], maybe not a function`);
   }
   obj[key] = function(...args: any[]) {
+    !hookFirst && oldFn.apply(this, args);
     try {
       fn.apply(this, args);
     } catch (error) {
       console.error(`[hook] function:${key} error`, error);
     }
-    oldFn.apply(this, args);
+    hookFirst && oldFn.apply(this, args);
   };
 }
